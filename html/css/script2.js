4886584fcb1d261d49875808ab14e71f45c1dcc5
ervinjohncalvo <calvoervinjohn@gmail.com>
2025-09-20 17:31:10 +0800

    https://www.w3schools.com/html/

---
+++ html/css/script2.js
@@ -0,0 +1,6 @@
+/* Contact Form Submission */
+document.getElementById("submit").addEventListener("click", subbutton);
+/* Displays an alert when the contact form is submitted.*/
+function subbutton() {
+  alert("Your message has been sent. Thank you!");
+}
